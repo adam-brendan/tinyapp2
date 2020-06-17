e23283adecdeb74042d679fded7c8b640b049451
Adam <adamcraveiro@gmail.com>
2020-06-17 19:59:23 -0400

    Add random generator as dependency, add form template

--- express_server.js
+++ express_server.js
@@ -1,33 +1,44 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+const randomString = require("randomstring");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-  };
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-  app.get("/", (req, res) => {
-    res.send("Hello!");
-  });
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
-  app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
-  app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-    res.render("urls_show", templateVars);
-  });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
 
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars);
+});
 
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}.`);
-  });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
